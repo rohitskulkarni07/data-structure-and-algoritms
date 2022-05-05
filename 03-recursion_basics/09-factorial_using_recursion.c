61d7017461596a7c32dca0ee794ecb2001c9682a
rohitkulkarni <rohitskulkarni07@gmail.com>
2022-05-05 22:25:11 +0530

    Added recursion code

---
+++ 03-recursion_basics/09-factorial_using_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned long long int fact(unsigned long long int);
+
+int main(void)
+{
+
+    unsigned long long int N = 0;
+    printf("Enter the number to find the factorial: ");
+    scanf("%llu", &N);
+    unsigned long long int result = fact(N);
+    printf("%llu\n", result);
+}
+
+unsigned long long int fact(unsigned long long int N)
+{
+    return (N == 0) ? 1 : (N * fact(N - 1));
+}
