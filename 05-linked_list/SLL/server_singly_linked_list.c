742eeeab0e8d874a019d02416b42a362d08bec78
rohitkulkarni <rohitskulkarni07@gmail.com>
2022-06-13 23:29:37 +0530

    Added SLL

---
+++ 05-linked_list/SLL/server_singly_linked_list.c
@@ -0,0 +1,331 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "singly_linked_list.h"
+
+list_t *create_list(void)
+{
+    node_t *p_head_node = NULL;
+    p_head_node = (node_t *)xmalloc(sizeof(node_t));
+    p_head_node->data = 0;
+    p_head_node->next;
+
+    return (p_head_node);
+}
+
+status_t insert_start(list_t *p_list, data_t new_data)
+{
+    generic_insert(p_list, get_node(new_data), p_list->next);
+    return (SUCCESS);
+}
+
+status_t insert_end(list_t *p_list, data_t new_data)
+{
+    node_t *p_last_node = NULL;
+    get_last_node(p_list, &p_last_node);
+    generic_insert(p_list, get_node(new_data), p_last_node);
+    return (SUCCESS);
+}
+
+status_t insert_after(list_t *p_list, data_t e_data, data_t new_data)
+{
+    node_t *p_e_node = NULL;
+    p_e_node = search_node(p_list, e_data);
+    if (p_e_node == NULL)
+    {
+        return (LIST_DATA_NOT_FOUND);
+    }
+    generic_insert(p_e_node, get_node(new_data), p_e_node->next);
+    return (SUCCESS);
+}
+
+status_t insert_before(list_t *p_list, data_t e_data, data_t new_data)
+{
+    node_t *p_e_node = NULL;
+    node_t *p_prev_node = NULL;
+
+    get_node_and_prev(p_list, e_data, &p_e_node, &p_prev_node);
+    if (p_e_node == NULL)
+    {
+        return (LIST_DATA_NOT_FOUND);
+    }
+    generic_insert(p_prev_node, get_node(new_data), p_e_node);
+    return (SUCCESS);
+}
+
+status_t get_start(list_t *p_list, data_t *p_start_data)
+{
+    if (is_empty(p_list) == TRUE)
+    {
+        return (LIST_EMPTY);
+    }
+    p_start_data = p_list->next->data;
+    return (SUCCESS);
+}
+
+status_t get_end(list_t *p_list, data_t *p_end_data)
+{
+    node_t *p_last_node = NULL;
+    if (is_empty(p_list) == TRUE)
+    {
+        return (LIST_EMPTY);
+    }
+    get_last_node(p_list, &p_last_node);
+    *p_end_data = p_last_node->data;
+    return (SUCCESS);
+}
+
+status_t pop_start(list_t *p_list, data_t *p_start_data)
+{
+    if (is_empty(p_list) == TRUE)
+    {
+        return (LIST_EMPTY);
+    }
+    *p_start_data = p_list->next->data;
+    generic_delete(p_list, p_list->next);
+    return (SUCCESS);
+}
+
+status_t pop_end(list_t *p_list, data_t *p_end_data)
+{
+    node_t *p_last_node = NULL;
+    node_t *p_last_node_prev = NULL;
+    if (is_empty(p_list) == TRUE)
+    {
+        return (LIST_EMPTY);
+    }
+    get_last_node_and_prev(p_list, &p_last_node, &p_last_node_prev);
+    *p_end_data = p_last_node->data;
+    generic_delete(p_last_node_prev, p_last_node);
+    return (SUCCESS);
+}
+
+status_t remove_start(list_t *p_list)
+{
+    if (is_empty(p_list) == TRUE)
+    {
+        return (LIST_EMPTY);
+    }
+    generic_delete(p_list, p_list->next);
+    return (SUCCESS);
+}
+
+status_t remove_end(list_t *p_list)
+{
+    node_t *p_last_node = NULL;
+    node_t *p_last_node_prev = NULL;
+    if (is_empty(p_list) == TRUE)
+    {
+        return (LIST_EMPTY);
+    }
+    get_last_node_and_prev(p_list, &p_last_node, &p_last_node_prev);
+    generic_delete(p_last_node_prev, p_last_node);
+    return (SUCCESS);
+}
+
+status_t remove_data(list_t *p_list, data_t r_data)
+{
+    node_t *p_data_node = NULL;
+    node_t *p_data_node_prev = NULL;
+    get_node_and_prev(p_list, r_data, &p_data_node, &p_data_node_prev);
+    if (is_empty(p_list) == TRUE)
+    {
+        return (LIST_EMPTY);
+    }
+    generic_delete(p_data_node_prev, p_data_node);
+    return (SUCCESS);
+}
+
+len_t get_length(list_t *p_list)
+{
+    node_t *p_run = NULL;
+    len_t len = 0;
+    while (p_run != NULL)
+    {
+        ++len;
+        p_run = p_run->next;
+    }
+    return (len);
+}
+
+bool is_empty(list_t *p_list)
+{
+    return (p_list->next == NULL);
+}
+
+bool is_member(list_t *p_list, data_t s_data)
+{
+    node_t *p_node = NULL;
+    p_node = p_list->next;
+    p_node = search_node(p_list, s_data);
+    if (p_node == NULL)
+    {
+        return (FALSE);
+    }
+    return (TRUE);
+}
+
+void show(list_t *p_list, const char *msg)
+{
+    node_t *p_run = NULL;
+
+    if (msg)
+    {
+        puts(msg);
+    }
+
+    printf("[ START ]->");
+    p_run = p_list->next;
+    while (p_run != NULL)
+    {
+        printf("[ %d ]->", p_run->data);
+        p_run = p_run->next;
+    }
+    printf("[ END ]->");
+}
+
+list_t *concat(list_t *p_list1, list_t *p_list2)
+{
+}
+
+list_t *merge(list_t *p_list1, list_t *p_list2)
+{
+}
+
+list_t *get_reversed_list(list_t *p_list)
+{
+}
+
+list_t *append(list_t *p_list1, list_t *p_list2)
+{
+}
+
+list_t *reversed_list(list_t *p_list)
+{
+}
+
+void to_array(list_t *p_list, data_t **p_array, size_t *p_size)
+{
+}
+
+list_t *to_list(data_t *p_array, size_t size)
+{
+}
+
+static destroy_list(list_t **pp_list)
+{
+    node_t *p_list = NULL;
+    node_t *p_run = NULL;
+    node_t *p_run_next = NULL;
+
+    p_list = *pp_list;
+    for (p_run = p_list->next; p_run != NULL; p_run_next)
+    {
+        p_run_next = p_run->next;
+        free(p_run);
+    }
+
+    free(p_list);
+    *pp_list = NULL;
+
+    return (SUCCESS);
+}
+
+static void generic_insert(node_t *p_beg, node_t *p_mid, node_t *p_end)
+{
+    p_mid->next = p_end;
+    p_beg->next = p_mid;
+}
+
+static void generic_delete(node_t *p_prev_node, node_t *p_delete_node)
+{
+    p_prev_node->next = p_delete_node->next;
+    free(p_delete_node);
+}
+
+static node_t *search_node(list_t *p_list, data_t s_data)
+{
+    node_t *p_run = NULL;
+    while (p_run != NULL)
+    {
+        if (p_run->data == s_data)
+        {
+            break;
+        }
+        p_run = p_run->data;
+    }
+    return (p_run);
+}
+
+static void get_last_node(list_t *p_list, node_t **pp_last_node)
+{
+    node_t *p_run = NULL;
+    p_run = p_list;
+
+    while (p_run != NULL)
+    {
+        p_run = p_run->next;
+    }
+    *pp_last_node = p_run;
+}
+
+static void get_last_node_and_prev(list_t *p_list, node_t **pp_node, node_t **pp_prev_node)
+{
+    node_t *p_run = NULL;
+    node_t *p_run_prev = NULL;
+    p_run = p_list;
+    p_run_prev = NULL;
+    while (p_run != NULL)
+    {
+        p_run_prev = p_run;
+        p_run = p_run->next;
+    }
+    *pp_node = p_run;
+    *pp_prev_node = p_run_prev;
+}
+static void get_node_and_prev(list_t *p_list, data_t s_data, node_t **pp_node, node_t **pp_prev_node)
+{
+    node_t *p_run = NULL;
+    node_t *p_run_prev = NULL;
+
+    p_run = p_list->next;
+    p_run_prev = p_list;
+
+    while (p_run != NULL)
+    {
+        if (p_run->data == s_data)
+        {
+            *pp_node = p_run;
+            *pp_prev_node = p_run_prev;
+            return;
+        }
+        p_run_prev = p_run;
+        p_run = p_run->next;
+    }
+    *pp_node = NULL;
+    *pp_prev_node = NULL;
+}
+
+static node_t *get_node(data_t new_data)
+{
+    node_t *p_node = NULL;
+
+    p_node = (node_t *)xmalloc(sizeof(node_t));
+    p_node->data = new_data;
+    p_node->next = NULL;
+
+    return (p_node);
+}
+
+static void *xmalloc(size_t nr_bytes)
+{
+    void *p = NULL;
+    p = malloc(nr_bytes);
+    if (p == NULL)
+    {
+        fprintf(stderr, "malloc: Out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+    return (p);
+}
---
+++ 05-linked_list/SLL/singly_linked_list.h
@@ -0,0 +1,74 @@
+#ifndef _SINGLY_LINKED_LIST_H
+#define _SINGLY_LINKED_LIST_H
+
+#include <stdio.h>
+
+#define SUCCESS 1
+#define TRUE 1
+#define FALSE 0
+#define LIST_DATA_NOT_FOUND 2
+#define LIST_EMPTY 3
+
+typedef int data_t;
+typedef int status_t;
+typedef int len_t;
+typedef int bool;
+typedef struct node node_t;
+typedef node_t list_t;
+
+/**
+ *
+ *             +------+-------+
+ *      node   | data | *next |
+ *             +------+-------+
+ */
+
+struct node
+{
+
+    data_t data;
+    struct node *next;
+};
+list_t *create_list(void);
+
+status_t insert_start(list_t *p_list, data_t new_data);
+status_t insert_end(list_t *p_list, data_t new_data);
+status_t insert_after(list_t *p_list, data_t e_data, data_t new_data);
+status_t insert_before(list_t *p_list, data_t e_data, data_t new_data);
+
+status_t get_start(list_t *p_list, data_t *p_start_data);
+status_t get_end(list_t *p_list, data_t *p_end_data);
+status_t pop_start(list_t *p_list, data_t *p_start_data);
+status_t pop_end(list_t *p_list, data_t *p_end_data);
+
+status_t remove_start(list_t *p_list);
+status_t remove_end(list_t *p_list);
+status_t remove_data(list_t *p_list, data_t r_data);
+
+len_t get_length(list_t *p_list);
+bool is_empty(list_t *p_list);
+bool is_member(list_t *p_list, data_t s_data);
+void show(list_t *p_list, const char *msg);
+
+list_t *concat(list_t *p_list1, list_t *p_list2);
+list_t *merge(list_t *p_list1, list_t *p_list2);
+list_t *get_reversed_list(list_t *p_list);
+
+list_t *append(list_t *p_list1, list_t *p_list2);
+list_t *reversed_list(list_t *p_list);
+
+void to_array(list_t *p_list, data_t **p_array, size_t *p_size);
+list_t *to_list(data_t *p_array, size_t size);
+
+static destroy_list(list_t** pp_list);
+
+static void generic_insert(node_t *p_beg, node_t *p_mid, node_t *p_end);
+static void generic_delete(node_t *p_prev_node, node_t *p_delete_node);
+static node_t *search_node(list_t *p_list, data_t s_data);
+static void get_node_and_prev(list_t *p_list, data_t s_data, node_t **pp_node, node_t **pp_prev_node);
+static void get_last_node(list_t *p_list, node_t **pp_last_node);
+static void get_last_node_and_prev(list_t *p_list, node_t **pp_node, node_t **pp_prev_node);
+static node_t *get_node(data_t new_data);
+static void *xmalloc(size_t nr_bytes);
+
+#endif
